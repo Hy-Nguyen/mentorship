e3fec56c0066707d73044a906cab87b4d3f4a58e
Miguel Uriostegui <muriostegui00@gmail.com>
2024-03-20 10:49:01 -0700

    update

--- app/uploadpage/page.tsx
+++ app/uploadpage/page.tsx
@@ -1,10 +1,69 @@
 import React from "react";
+import { Button } from "@nextui-org/react";
 
-
-export default function uploadpage() {
+export default function App() {
   return (
-  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Resume
-    </button>
+
+
+
+
+
+    // BUTTONS NEXT.js SAMPLE
+    <div className="flex flex-wrap gap-4 items-center">
+      <Button color="primary" variant="solid">
+        Solid
+      </Button>
+      <Button color="primary" variant="faded">
+        Faded
+      </Button>
+      <Button color="primary" variant="bordered">
+        Bordered
+      </Button>
+      <Button color="primary" variant="light">
+        Light
+      </Button>
+      <Button color="primary" variant="flat">
+        Flat
+      </Button>
+      <Button color="primary" variant="ghost">
+        Ghost
+      </Button>
+      <Button color="primary" variant="shadow">
+        Shadow
+      </Button>
+
+
+
+
+
+
+
+      {/* RESUME*/}
+      <div className="relative w-48 h-64 rounded-lg bg-white shadow-md">
+        {/* Resume image placeholder */}
+        <div className="w-full h-3/4 rounded-t-lg bg-gray-200"></div>
+
+        {/* Resume Button */}
+        <div className="absolute bottom-4 left-4">
+          <Button color="primary" variant="solid" size="sm">
+            Preview Resume
+          </Button>
+        </div>
+      </div>
+
+
+      {/* TRANSCRIPT */}
+      <div className="relative w-48 h-64 rounded-lg bg-white shadow-md">
+        {/* Transcript image palcehodler */}
+        <div className="w-full h-3/4 rounded-t-lg bg-gray-200"></div>
+
+        {/* Transcript Button */}
+        <div className="absolute bottom-4 left-4">
+          <Button color="primary" variant="solid" size="sm">
+            Preview Transcript
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }

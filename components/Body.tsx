600d430ac50596e31c7bd2a2f96b098894e5ffff
Hy Nguyen <giahy316@gmail.com>
2024-03-20 13:44:49 -0700

    Updated Layout

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,13 +1,28 @@
+import Appointments from "./Appointments";
+import Calendar from "./Calendar";
+import Chat from "./Chat";
+import Mentors from "./Mentors";
+import QuickAction from "./QuickAction";
+import UserProfile from "./UserProfileDash";
+
 export default function Body() {
   return (
-    <>
-      <div>
-        <div>
+    <div className="flex-col flex w-full">
+      <div className="flex flex-row w-full h-3/5">
+        <div className="flex flex-col w-5/6 h-full bg-amber-300">
           <QuickAction />
-          <MyMentors />
+          <Mentors />
         </div>
-        <UserProfile />
+
+        <div className="h-full w-2/3 bg-blue-300">
+          <UserProfile />
+        </div>
       </div>
-    </>
+      <div className="bg-slate-500 h-full flex">
+        <Appointments />
+        <Calendar />
+        <Chat />
+      </div>
+    </div>
   );
 }
---
+++ components/Calendar.tsx
@@ -0,0 +1,16 @@
+import {
+  Card,
+  CardBody,
+} from "@nextui-org/react";
+
+export default function Calendar() {
+  return (
+    <div className="flex items-center justify-center bg-purple-400 w-1/3">
+      <Card className=" m-4">
+        <CardBody>
+          <h1>Calendar</h1>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}

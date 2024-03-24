e4fe1803f56ec4fc24b09c53268cbae027ce0bd8
Miguel Uriostegui <muriostegui00@gmail.com>
2024-03-24 13:57:57 -0700

    Created Visual Cards for Resume & Transcript (In the works)

--- app/uploadpage/page.tsx
+++ app/uploadpage/page.tsx
@@ -1,69 +1,61 @@
 import React from "react";
+import Image from "next/image";
+// import NavBar from "../../components/NavBar";
+import SideBar from "../../components/SideBar";
+// import Body from "../../components/Body";
 import { Button } from "@nextui-org/react";
+import {
+  Avatar,
+  Card,
+  CardBody,
+  CardHeader,
+} from "@nextui-org/react";
+
+
+
+// ================== IGNORE WIERD COLORS AND BORDERS FOR NOW ===================
 
 export default function App() {
   return (
+    <>
+      <div className="flex">
+        <SideBar />
 
+        <div className="flex flex-wrap justify-center">
 
+          {/* RESUME CARD */}
+          <Card className="w-1/3 m-2 border-4 border-red-500 p-0 bg-yellow-200 flex flex-col justify-center items-center">
+            {/* HEADER */}
+            <CardHeader className="w-11/12 m-4 border-4 border-green-500 p-0 rounded-none">
+              <img src="/sampleResume.png" alt="Preview of Transcript" />
+            </CardHeader>
+            {/* BODY */}
+            <CardBody className="w-auto h-100% my-2 border-4 border-blue-500 p-0 flex justify-center items-center">
+              <div>
+                <Button color="primary" variant="solid" size="lg">
+                  Preview Resume
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
 
-
-
-    // BUTTONS NEXT.js SAMPLE
-    <div className="flex flex-wrap gap-4 items-center">
-      <Button color="primary" variant="solid">
-        Solid
-      </Button>
-      <Button color="primary" variant="faded">
-        Faded
-      </Button>
-      <Button color="primary" variant="bordered">
-        Bordered
-      </Button>
-      <Button color="primary" variant="light">
-        Light
-      </Button>
-      <Button color="primary" variant="flat">
-        Flat
-      </Button>
-      <Button color="primary" variant="ghost">
-        Ghost
-      </Button>
-      <Button color="primary" variant="shadow">
-        Shadow
-      </Button>
-
-
-
-
-
-
-
-      {/* RESUME*/}
-      <div className="relative w-48 h-64 rounded-lg bg-white shadow-md">
-        {/* Resume image placeholder */}
-        <div className="w-full h-3/4 rounded-t-lg bg-gray-200"></div>
-
-        {/* Resume Button */}
-        <div className="absolute bottom-4 left-4">
-          <Button color="primary" variant="solid" size="sm">
-            Preview Resume
-          </Button>
+          {/* TRANSCRIPT CARD */}
+          <Card className="w-1/3 m-2 border-4 border-red-500 p-0 bg-yellow-200 flex flex-col justify-center items-center">
+            {/* HEADER */}
+            <CardHeader className="w-11/12 m-4 border-4 border-green-500 p-0 rounded-none">
+              <img src="/sampleResume.png" alt="Preview of Transcript" />
+            </CardHeader>
+            {/* BODY */}
+            <CardBody className="w-auto h-100% my-2 border-4 border-blue-500 p-0 flex justify-center items-center">
+              <div>
+                <Button color="primary" variant="solid" size="lg">
+                  Preview Transcript
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
         </div>
       </div>
-
-
-      {/* TRANSCRIPT */}
-      <div className="relative w-48 h-64 rounded-lg bg-white shadow-md">
-        {/* Transcript image palcehodler */}
-        <div className="w-full h-3/4 rounded-t-lg bg-gray-200"></div>
-
-        {/* Transcript Button */}
-        <div className="absolute bottom-4 left-4">
-          <Button color="primary" variant="solid" size="sm">
-            Preview Transcript
-          </Button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }

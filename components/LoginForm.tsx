0236a6d440f23451780dd979d8972235d53f6555
Hy Nguyen <giahy316@gmail.com>
2024-03-24 15:35:41 -0700

    Fixes

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,21 +1,36 @@
 "use client";
-export default function LoginForm() {
 
+import React from "react";
+
+export default function LoginForm() {
   return (
     <form action="/login" method="post">
-        <h2>Login Page</h2>
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required>
-        </div>
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-        <div>
-            <button type="submit">Login</button>
-        </div>
+      <h2>Login Page</h2>
+      <div>
+        <label htmlFor="username">
+          Username:
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          required
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="password">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+        ></input>
+      </div>
+      <div>
+        <button type="submit">Login</button>
+      </div>
     </form>
-)
-
+  );
 }

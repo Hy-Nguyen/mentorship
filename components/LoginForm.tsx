062249e0ca1120d7574e35614332c30b416ca0e9
yalsanna <157405876+yalsanna@users.noreply.github.com>
2024-03-24 14:47:58 -0700

    LoginFrom Update
    Login in page implemetned (errors are present)

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,4 +1,21 @@
 "use client";
 export default function LoginForm() {
-  return <h1>User Name: </h1>;
+
+  return (
+    <form action="/login" method="post">
+        <h2>Login Page</h2>
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+)
+
 }

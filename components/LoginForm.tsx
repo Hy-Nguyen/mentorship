f103e954dd19292f40b86aeac8cd980622b8b488
yalsanna <157405876+yalsanna@users.noreply.github.com>
2024-03-24 15:49:03 -0700

    Upload

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,32 +1,18 @@
 "use client";
 
 import React from "react";
-
+import "./Login.css";
 export default function LoginForm() {
   return (
     <form action="/login" method="post">
       <h2>Login Page</h2>
       <div>
-        <label htmlFor="username">
-          Username:
-        </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          required
-        ></input>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required></input>
       </div>
       <div>
-        <label htmlFor="password">
-          Password:
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          required
-        ></input>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required></input>
       </div>
       <div>
         <button type="submit">Login</button>

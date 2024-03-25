a4fe0b12c123fb3daa51aceb911f6c785c9868e7
Hy Nguyen <giahy316@gmail.com>
2024-03-24 20:57:58 -0700

    cardFooter

--- app/uploadpage/page.tsx
+++ app/uploadpage/page.tsx
@@ -9,9 +9,8 @@ import {
   Card,
   CardBody,
   CardHeader,
+  CardFooter,
 } from "@nextui-org/react";
-
-
 
 // ================== IGNORE WIERD COLORS AND BORDERS FOR NOW ===================
 
@@ -22,17 +21,23 @@ export default function App() {
         <SideBar />
 
         <div className="flex flex-wrap justify-center">
-
           {/* RESUME CARD */}
           <Card className="w-1/3 m-2 border-4 border-red-500 p-0 bg-yellow-200 flex flex-col justify-center items-center">
             {/* HEADER */}
             <CardHeader className="w-11/12 m-4 border-4 border-green-500 p-0 rounded-none">
-              <img src="/sampleResume.png" alt="Preview of Transcript" />
+              <img
+                src="/sampleResume.png"
+                alt="Preview of Transcript"
+              />
             </CardHeader>
             {/* BODY */}
             <CardBody className="w-auto h-100% my-2 border-4 border-blue-500 p-0 flex justify-center items-center">
               <div>
-                <Button color="primary" variant="solid" size="lg">
+                <Button
+                  color="primary"
+                  variant="solid"
+                  size="lg"
+                >
                   Preview Resume
                 </Button>
               </div>
@@ -43,16 +48,23 @@ export default function App() {
           <Card className="w-1/3 m-2 border-4 border-red-500 p-0 bg-yellow-200 flex flex-col justify-center items-center">
             {/* HEADER */}
             <CardHeader className="w-11/12 m-4 border-4 border-green-500 p-0 rounded-none">
-              <img src="/sampleResume.png" alt="Preview of Transcript" />
+              <img
+                src="/sampleResume.png"
+                alt="Preview of Transcript"
+              />
             </CardHeader>
             {/* BODY */}
-            <CardBody className="w-auto h-100% my-2 border-4 border-blue-500 p-0 flex justify-center items-center">
+            <CardFooter className="w-auto h-100% my-2 border-4 border-blue-500 p-0 flex justify-center items-center">
               <div>
-                <Button color="primary" variant="solid" size="lg">
+                <Button
+                  color="primary"
+                  variant="solid"
+                  size="lg"
+                >
                   Preview Transcript
                 </Button>
               </div>
-            </CardBody>
+            </CardFooter>
           </Card>
         </div>
       </div>

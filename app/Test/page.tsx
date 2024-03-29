fcac7a88c96e293aef5904ed9fdb803c7a30973f
Crispy404 <65375799+Crispy404@users.noreply.github.com>
2024-03-28 17:01:43 -0700

    input values

---
+++ app/Test/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+    Button,
+    Card,
+    CardBody,
+  } from "@nextui-org/react";
+  import { useState } from "react";
+  
+  export default function Home() {
+    const [editView, setEditView] = useState(true);
+  
+    return (
+      <>
+        <div className="flex min-h-screen flex-col justify-center items-center space-y-4 ">
+          {editView ? (
+            // TRUE CASE
+            <Card className="dark text-3xl w-1/2 bg-slate-200">
+              <CardBody>
+                <h1 className="font-bold text-black w-full text-center ">
+                  Show Profile
+                </h1>
+              </CardBody>
+            </Card>
+          ) : (
+            // FALSE CASE
+            <Card className="dark text-3xl w-1/2">
+              <CardBody>
+                <h1 className="font-bold text-white w-full text-center ">
+                  Edit Profile
+                </h1>
+              </CardBody>
+            </Card>
+          )}
+          <Button
+            onClick={() => setEditView(!editView)}
+          >
+            Edit
+          </Button>
+        </div>
+      </>
+    );
+  }

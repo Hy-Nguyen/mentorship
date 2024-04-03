242ebc1b57f77a2c125f66ef68291b0c63956745
Crispy404 <65375799+Crispy404@users.noreply.github.com>
2024-04-03 09:59:26 -0700

    Update TagsCard.tsx

--- components/profile/TagsCard.tsx
+++ components/profile/TagsCard.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { Interests } from "@mui/icons-material";
+import { Toolbar, Tooltip, TooltipProps } from "@mui/material";
 import {
   Card,
   CardBody,
+  RadioGroup,
+  Radio,
 } from "@nextui-org/react";
 
 export default function Tags() {
+  const interests = ["Academic", "Professional","tah2","tag3"]
+
   return (
     <div className="flex-col flex w-3/5 space-y-4">
       <div>
@@ -14,7 +20,25 @@ export default function Tags() {
         </h1>
       </div>
       <Card className=" bg-gray-50 w-full h-full  m-2 mt-0 marker:float-left text-black ">
-        <CardBody></CardBody>
+        <CardBody>
+          {/* Create map for Radios */}
+
+          <RadioGroup>
+            {interests.map((interest,i) => (
+
+
+              <Radio
+                key={i}
+                value={interest}
+                className="text-black"
+                >
+                  {interest}
+              </Radio>
+            
+            ))}
+
+          </RadioGroup>
+        </CardBody>
       </Card>
     </div>
   );

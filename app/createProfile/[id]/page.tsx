6651278109186fa898224196dc029a7c982823f0
Hy Nguyen <giahy316@gmail.com>
2024-04-12 19:34:18 -0700

    updates

--- app/createProfile/[id]/page.tsx
+++ app/createProfile/[id]/page.tsx
@@ -1,6 +1,13 @@
 import SideBar from "@/components/sidebar/SideBar";
 import { createClient } from "@/utils/supabase/client";
 import CreateProfileForm from "@/components/profile/CreateProfile";
+import {
+  Card,
+  CardBody,
+  Spinner,
+} from "@nextui-org/react";
+
+import { redirect } from "next/navigation";
 
 export default async function CreateProfile({
   params,
@@ -14,7 +21,7 @@ export default async function CreateProfile({
     try {
       const { data: User, error } = await supabase
         .from("Users")
-        .select("name, role, email")
+        .select("UserID, name, role, email")
         .eq("UserID", id);
 
       return User![0];
@@ -25,19 +32,61 @@ export default async function CreateProfile({
     return user;
   }
 
+  async function checkProfile() {
+    const supabase = createClient();
+    const id = params.id;
+
+    try {
+      const { data: User, error } = await supabase
+        .from("Profiles")
+        .select()
+        .eq("id", id);
+
+      if (User?.length != 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   let user: {
+    UserID: number;
     name: string;
     role: string;
     email: string;
   } = await getUser();
 
+  if (await checkProfile()) {
+    redirect(`/profile/${user.UserID}`);
+  }
+
   return (
     <div className="flex bg-white">
       <SideBar userID={params.id} />
 
-      <div className=" flex flex-col w-full items-center pt-10 space-y-2">
-        <p className="text-black">hi</p>
-        <CreateProfileForm user={user} />
+      <div className="flex flex-col w-full items-center pt-[10%] space-y-2">
+        {!(await checkProfile()) ? (
+          <CreateProfileForm user={user} />
+        ) : (
+          <>
+            <div className="flex flex-col w-full items-center pt-[10%] space-y-2">
+              <Card className="w-1/2 h-[100px] ">
+                <CardBody className="flex justify-center items-center">
+                  <h1 className="text-[24px] font-bold">
+                    Profile Already Created!
+                  </h1>
+                  <p>
+                    Redirecting to Profile Page
+                  </p>
+                  <Spinner />
+                </CardBody>
+              </Card>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- components/profile/CreateProfile.tsx
+++ components/profile/CreateProfile.tsx
@@ -6,11 +6,13 @@ import {
   CardBody,
   Button,
   CardHeader,
+  CardFooter,
 } from "@nextui-org/react";
 import { useState } from "react";
 
 export default function CreateProfileForm(props: {
   user: {
+    UserID: number;
     name: string;
     role: string;
     email: string;
@@ -27,93 +29,224 @@ export default function CreateProfileForm(props: {
   const [city, setCity] = useState("");
   const [state, setState] = useState("");
 
+  const [email, setEmail] = useState(
+    props.user.email
+  );
+  const [website, setWebsite] = useState("");
+  const [github, setGithub] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const [twitter, setTwitter] = useState("");
+
+  async function handleSubmit(
+    e: React.FormEvent
+  ) {
+    e.preventDefault();
+
+    var splitName = name.split(" ");
+
+    let reqBody = {
+      id: props.user.UserID,
+      fName: splitName[0],
+      lName: splitName[1],
+      email: email,
+      phone: phone,
+      organization: organization,
+      job: title,
+      address: {
+        city: city,
+        state: state,
+        street: "",
+        zipCode: "",
+      },
+      socials: {
+        github: github,
+        twitter: twitter,
+        website: website,
+        linkedIn: linkedin,
+      },
+    };
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/profiles/create/${props.user.UserID}`,
+        {
+          cache: "no-store",
+          method: "POST",
+          body: JSON.stringify(reqBody),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          "Network response was not ok"
+        );
+      }
+      const data = await response.json();
+      alert(data.message);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <>
       <div className="flex bg-white">
-        <Card className="flex justify-center items-center">
-          <CardHeader className=" flex justify-center items-center ">
-            <h1 className="text-[24px] font-bold mt-[20px] mb-[-20px]">
-              User Info
-            </h1>
-          </CardHeader>
-          <CardBody className="flex justify-center">
-            <form>
-              <div className=" px-10 pt-10 space-y-4 flex flex-col ">
-                <div className="flex flex-row space-x-2">
-                  <Input
-                    type="string"
-                    value={name}
-                    label="Name"
-                    labelPlacement="outside"
-                    className="w-1/2"
-                    isReadOnly
-                  />
-
-                  <Input
-                    type="tel"
-                    value={phone}
-                    onValueChange={setPhone}
-                    label="Phone Number"
-                    labelPlacement="outside"
-                    className="w-1/2"
-                  />
-                </div>
-                <div className=" flex justify-between items-end">
-                  <Input
-                    type="string"
-                    value={organization}
-                    onValueChange={
-                      setOrganization
-                    }
-                    label="Organization"
-                    labelPlacement="outside"
-                    className=""
-                  />
-                </div>
-                <div className=" flex justify-between items-end">
-                  <Input
-                    type="string"
-                    value={title}
-                    onValueChange={setTitle}
-                    label="Current Title"
-                    labelPlacement="outside"
-                    className=""
-                  />
-                </div>
-                <div className=" flex justify-between items-end">
-                  <div className="flex flex-row space-x-2 w-full">
+        <form onSubmit={handleSubmit}>
+          <Card className="flex  justify-center items-center">
+            <CardHeader className=" flex justify-center items-center ">
+              <h1 className="text-[24px] font-bold mt-[20px] mb-[-20px]">
+                User Info
+              </h1>
+            </CardHeader>
+            <div className="flex flex-row">
+              <CardBody className="flex justify-center">
+                <div className=" px-10 pt-10 space-y-4 flex flex-col ">
+                  <div className="flex flex-row space-x-2">
                     <Input
                       type="string"
-                      value={city}
-                      onValueChange={setCity}
-                      label="City"
+                      value={name}
+                      label="Name"
                       labelPlacement="outside"
                       className="w-1/2"
+                      isReadOnly
                     />
                     <Input
-                      type="string"
-                      value={state}
-                      onValueChange={setState}
-                      label="State"
+                      type="tel"
+                      value={phone}
+                      onValueChange={setPhone}
+                      label="Phone Number"
                       labelPlacement="outside"
                       className="w-1/2"
                     />
                   </div>
+                  <div className=" flex justify-between items-end">
+                    <Input
+                      type="string"
+                      value={organization}
+                      onValueChange={
+                        setOrganization
+                      }
+                      label="Organization"
+                      labelPlacement="outside"
+                      className=""
+                    />
+                  </div>
+                  <div className=" flex justify-between items-end">
+                    <Input
+                      type="string"
+                      value={title}
+                      onValueChange={setTitle}
+                      label="Current Title"
+                      labelPlacement="outside"
+                      className=""
+                    />
+                  </div>
+                  <div className=" flex justify-between items-end">
+                    <div className="flex flex-row space-x-2 w-full">
+                      <Input
+                        type="string"
+                        value={city}
+                        onValueChange={setCity}
+                        label="City"
+                        labelPlacement="outside"
+                        className="w-1/2"
+                      />
+                      <Input
+                        type="string"
+                        value={state}
+                        onValueChange={setState}
+                        label="State"
+                        labelPlacement="outside"
+                        className="w-1/2"
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="w-full space-x-2 flex justify-center">
-                  <Button
-                    className="w-1/2"
-                    color="success"
-                    variant="shadow"
-                    type="submit"
-                  >
-                    Create
-                  </Button>
+              </CardBody>
+              <CardBody>
+                <div className=" px-10 pt-10 space-y-4 flex flex-col ">
+                  <div className="flex flex-row space-x-2">
+                    <Input
+                      type="email"
+                      value={email}
+                      onValueChange={setEmail}
+                      label="E-Mail"
+                      labelPlacement="outside"
+                      className="w-full"
+                      isReadOnly
+                    />
+                    <Input
+                      type="string"
+                      value={website}
+                      onValueChange={setWebsite}
+                      label="Website"
+                      labelPlacement="outside"
+                      className="w-full"
+                    />
+                  </div>
+
+                  <div className=" flex justify-between items-end">
+                    <Input
+                      type="string"
+                      value={github}
+                      onValueChange={setGithub}
+                      label="GitHub"
+                      labelPlacement="outside"
+                      className="w-full"
+                      startContent={
+                        <p className="text-sm">
+                          https://github.com/
+                        </p>
+                      }
+                    />
+                  </div>
+                  <div className=" flex justify-between items-end">
+                    <Input
+                      type="string"
+                      value={linkedin}
+                      onValueChange={setLinkedin}
+                      label="LinkedIn"
+                      labelPlacement="outside"
+                      className="w-full"
+                      startContent={
+                        <p className="text-sm">
+                          https://linkedin.com/in/
+                        </p>
+                      }
+                    />
+                  </div>
+                  <div className=" flex justify-between items-end">
+                    <Input
+                      type="string"
+                      value={twitter}
+                      onValueChange={setTwitter}
+                      label="Twitter"
+                      labelPlacement="outside"
+                      className="w-full"
+                      startContent={
+                        <p className="text-sm">
+                          https://twitter.com/
+                        </p>
+                      }
+                    />
+                  </div>
                 </div>
+              </CardBody>
+            </div>
+            <CardFooter>
+              <div className="w-full space-x-2 my-8 flex justify-center">
+                <Button
+                  className="w-1/3 text-black hover:text-white"
+                  color="success"
+                  variant="shadow"
+                  type="submit"
+                >
+                  Create
+                </Button>
               </div>
-            </form>
-          </CardBody>
-        </Card>
+            </CardFooter>
+          </Card>
+        </form>
       </div>
     </>
   );
